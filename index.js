47016f0ae9b788bf9e1a75591f45ce25833429ab
VergHar <vergsev@mail.ru>
2020-07-19 19:48:59 +0300

    js-variables

---
+++ index.js
@@ -0,0 +1,56 @@
+//alert("hello");
+var a=5
+let b=1
+const c=6
+console.log(a, b, c);
+let t=5
+console.log(t)
+t= ("barev") ;
+console.log(t);
+//let foo=7;
+//console.log(foo);
+let foo;
+console.log(foo);
+let trueBool= true;
+let falsBool= false;
+console.log(trueBool);
+/*let n=78;
+let n1=1.6;
+let n2=0.2;
+let n3=Infinity;
+let n4=NaN;
+let str=hi;
+let str1="hi";
+let str2='hi';
+const name = prompt('Hi,please enter your name');
+const age=prompt('please,enter your age');
+alert('Hi'+ name+ 'yuo are' + age + 'years old');*/
+
+console.log(typeof true );
+console.log(typeof Infinity);
+console.log(typeof undefined);
+let zoo=null;
+console.log(zoo);
+//'anun azganun'->012345678
+const obj= {
+    a: 5, b: 6, c: 7
+}
+console.log(obj);
+const tellNumbers={
+    narek: 1234,
+    mika: 5678
+}
+console.log(tellNumbers);
+console.log(tellNumbers.mika);
+const p = {
+    firstName:'John',
+    lastNAme: 'Lou',
+    age: 22,
+    married: true
+};
+console.log(p);
+console.log(`${p.firstName} ${p.lastNAme}`)
+console.log(p.car);
+//ssconsole.log('age',  {age});
+const arr =[5, 6, true, false, String,]
+console.log(arr[0] );
